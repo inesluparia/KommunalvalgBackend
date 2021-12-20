9d62ddf941d298a5ba2d893e3ebf7ca8d688ae27
inesluparia <inesluparia@gmail.com>
2021-12-20 12:20:51 +0100

    Added data for candidates

--- src/main/java/eksamen/model/Candidate.java
+++ src/main/java/eksamen/model/Candidate.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Candidate {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,30 @@
 INSERT INTO political_parties (abbreviation, name) VALUES
-('Ø', 'Enhedslisten'),
+('Ø', 'Enhedslisten, De Rød Grønne'),
 ('A', 'Socialdemokratiet'),
+('C', 'Det konservative Folkeparti'),
 ('V', 'Venstre, Danmarks Liberale Parti'),
 ('O', 'Dansk Folkeparti'),
-('Ø', 'Enhedslisten + De Rød Grønne'),
 ('F-SF', 'Socialistisk Folkeparti');
+
+INSERT INTO candidates (name, political_party_id) VALUES
+('Marcel Meijer', '2'),
+('Michael Kristensen','2'),
+('Helle Hansen', '2'),
+('Karina Knobelauch', '2'),
+
+('Stefan Hafstein Wolffbrandt', '3'),
+('Robert V. Rasmussen','3'),
+('Pia Ramsing', '3'),
+
+('Ulla Holm', '6'),
+('Kjeld Bønkel','6'),
+('Anne Grethe Olsen', '6'),
+
+('Per Mortensen', '5'),
+
+('Søren Wiese','4'),
+('Carsten Bruun', '4'),
+
+('Jette M. Søgaard','1'),
+('Pia Birkmand', '1')
+;
